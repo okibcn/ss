a5cc77ad6b09e4104000bd2425f76096d86532b4
Mioki <22417711+okibcn@users.noreply.github.com>
2023-02-08 00:44:44 -0800

    Readme initialization

--- README.md
+++ README.md
@@ -1,2 +1,167 @@
-# ss
-Scoop Super Search, instantaneous results, UTF-8 and regex compatible.
+# SS (Scoop Super Search) 
+
+<br/>
+
+Scoop Super Search, instantaneous results, UTF-8 and regex compatible. The fastest search engine for Scoop.
+
+<br/>
+
+
+____
+
+## FEATURES
+
+<br/>
+
+### - 🛸 'Outta This World' speed
+
+<br/>
+
+**Scoop Super Search** is capable of searching in more than 800 buckets and 52,000 app manifests in internet. It uses the lightning fast ScoopMaster database to provide intantaneous results in less than 500 ms.
+
+<br/>
+
+
+### - 🔍 Searches in names AND descriptions improving the search flexibility.
+<br/>
+Most Scoop search systems such as [Scoop.sh](http://scoop.sh) or [Scoop-Directory](https://rasa.github.io/scoop-directory/search) provide results only referred to the app name. SS can search also in the app descriptions.
+
+<br/>
+
+
+### - 🔍&🔍 Match All keywords capability. 
+<br/>
+Most Scoop search systems provide results for a single keyword. SS can search combinations of OR and AND.
+
+<br/>
+
+
+### - 🔧 Complex REGEX search patterns for advanced users
+<br/>
+
+**SS** is also cabable of extended **REGEX** pattern seaches, accepting even multiple regex patterns.
+
+<br/>
+
+
+### - (音乐) UTF-8 compatible
+<br/>
+
+**SS** accepts searches in UTF-8 encoding, supporting searches in descriptions when the language uses suplemental Unicode pages. Please note that this feature requires a UTF-8 capable terminal such as Windows Terminal.
+
+<br/>
+
+
+### - 📝 Last-manifest filter
+<br/>
+
+Typical search utilities provide only the list of matches including all the versions and all the manifests for that app. **SS** can also filter out all the noise in the report, displaying only the latests manifest of the highest version of each app.
+
+<br/>
+
+
+### - 📝 Interoperability with other PowerShell scripts
+<br/>
+
+**SS** can provide the output as a PSObject format so other PowerShell utilities could use the data for other tasks.
+
+<br/>
+
+____
+
+<br/>
+
+## INSTALLATION
+
+<br/>
+
+This app is a CLI utility for the Scoop framework and requires [Scoop ackage Manager](http://scoop.sh) as a pre-requisite.
+
+From there the installation is straightforward.
+
+1. Add the home bucket for this utility:
+```pwsh
+scoop bucket add .oki. http://github.com/okibcn/Bucket
+```
+2. Install the app:
+```pwsh
+scoop install ss
+```
+
+App update:
+```pwsh
+scoop update ss
+```
+
+App uninstall:
+```pwsh
+scoop uninstall ss
+```
+
+<br/>
+
+____
+
+<br/>
+
+## USAGE
+
+<br/>
+
+Usage: ss [OPTIONS] [Patterns]
+
+ **SS** searches in all the known buckets at a lighning speed. It not only searches in the name field, but also in the desscription. Regex and UTF-8 compatible.
+```
+ Options:
+
+  no opt. searches for all the matches in the name and description fields.
+     -n   Searches only in the name field.
+     -s   Simple search. searches an exact name match (implies -n).
+     -e   Full expanded regex search.
+     -l   Search latest versions only
+     -r   raw, no color and no header. Outputs data as a PowerShell object.
+     -h   Prints this help
+```
+
+<br/>
+
+____
+
+<br/>
+
+## EXAMPLES
+
+<br/>
+
+- Search for all the packages with both words in the name or description:
+```pwsh
+ss scoop search
+```
+- Search for an app in which the name contains both 'nvidia' AND 'driver'
+```pwsh
+ss -n nvidia driver 
+```
+- Simple search for the **ss** app
+```pwsh
+ss -s ss 
+```
+- Returns apps containing 'tool' and, 'nvidia' or 'radeon'
+```pwsh
+ss -n "nvidia|radeon" tool
+```
+- Get the latest manifests of scoop search utilities
+```pwsh
+ss -l search scoop 
+```
+- latests versions of apps ending in 'ss' starting with 's'
+```pwsh
+ss -n -l -e ss$ ^s 
+```
+- UTF-8 search of all the apps containing the word 音乐 (music) in the description.
+```pwsh
+ss -l 音乐 
+```
+- stores in the `$apps` variable a PSObject with all the Scoop manifests — more than 52,000.
+```pwsh
+`$apps = ss -r .* 
+```
---
+++ ss/ss.ps1
@@ -0,0 +1,119 @@
+#   Scoop Super Search v1.0 2023.02.07
+#   (C) 2023 Oscar Lopez
+#   For more information visit: https://github.com/okibcn/ss"
+
+
+## Finds texts in local scoop database
+function ss {
+    $cNormal = "$([char]27)[37m"  # White
+    $cMatch = "$([char]27)[33m"  # Yellow
+    $cBucket = "$([char]27)[36m"  # Cyan
+    $oHelp = $oName = $oExact = $oLast = $oRaw = $false
+    $pattern = foreach ($arg in $args) {
+        $arg = [string]$arg
+        if (($arg[0] -eq '-') -AND ($arg[2] -eq $null)) {
+            switch ($arg[1]) {
+                'n' { $oName = $true  ; break }
+                'l' { $oLast = $true  ; break }
+                'h' { $oHelp = $true  ; break }
+                'e' { $oSimple = $true ; break }
+                'r' { $oRaw = $true  ; break }
+                's' { 
+                    $oExact = $true
+                    $oName = $true 
+                }
+            }
+        }
+        else {
+            $arg
+        }
+    }
+    if (($oHelp) -OR (!$oRaw)) {
+        Write-Host " Scoop Super Search v0.1 2023.02.07
+ (C) 2023 Oscar Lopez
+ ss -h for help. For more information visit: https://github.com/okibcn/ss"
+    }
+    if (($oHelp) -OR ($pattern.count -eq 0)) {
+        Write-Host "
+ Usage: ss [OPTIONS] [Patterns]
+
+ ss searches in all the known buckets at a lighning speed. It not only searhes 
+ in the name field, but also in the desscription. Regex and UTF-8 compatible.
+
+ Options:
+
+  no opt. searches for all the matches in the name and description fields.
+     -n   Searches only in the name field.
+     -s   Simple search. searches an exact name match (implies -n).
+     -e   Full expanded regex search.
+     -l   Search latest versions only
+     -r   raw, no color and no header. Outputs data as a PowerShell object.
+     -h   Prints this help
+
+ Examples:
+
+     ss scoop search (all the packages with both words in the name or description)
+     ss -n nvidia driver (the name contains both 'nvidia' AND 'driver')
+     ss -n `"nvidia|radeon`" tool  (contains 'tool' and, 'nvidia' or 'radeon')
+     ss -l search scoop (the latest manifests of scoop search utilities)
+     ss -n -l -e ss$ ^s (latests versions of apps ending in 'ss' starting with 's')
+     ss -l 音乐 (UTF-8 search)
+     `$apps = ss -r .*  (stores in `$apps a PSObject with all the Scoop manifests)
+
+     "
+        return
+    }
+
+    $DBfile = "$($env:TEMP)/AllAppsDB.7z"
+    if ((-NOT (test-path $DBfile)) -OR (((Get-Date) - (gci $DBfile).LastWriteTime).Minutes -ge 30)) {
+        aria2c --allow-overwrite=true https://github.com/okibcn/ScoopMaster/releases/download/Databases/AllAppsDB.7z -d "$env:TEMP" | Out-Null
+    }
+    $csv = 7z e -so $DBfile | select-object -skip 1
+    if ($oLast) {
+        $csv = $csv | Select-String -Pattern "okibcn/ScoopMaster" -raw
+    }
+    if ($oExact) {
+        # Exact name match
+        $csv = $csv | Select-String -Pattern "^`"$pattern`"" -raw
+        if (!$csv) { return }
+        $table = (echo '"Name","Version","Date","Bucket","Description"'$csv) | ConvertFrom-Csv
+    }
+    else {
+        # regex search Yes/No
+        if (!$oSimple) {
+            $pattern | % { $csv = $csv | Select-String -pattern "$_" -raw }
+        }
+        if (!$csv) { return }
+        $table = (echo '"Name","Version","Date","Bucket","Description"'$csv) | ConvertFrom-Csv
+        if ($oName) {
+            # search name field only
+            $pattern | % { 
+                $pattern_ = $_
+                $table = $table | ? { ($($_.Name) -match "$pattern_" ) }
+            }
+        }
+        else {
+            # search name and description
+            $pattern | % { 
+                $pattern_ = $_
+                $table = $table | ? { ($($_.Name) -match "$pattern_" ) -OR ($($_.Description) -match "$pattern_") }
+            }
+        }
+    }
+    $table | % { $_.bucket = "http://github.com/$($_.bucket)" }
+    if ($oRaw) {
+        return ($table | Select-Object Name, Version, Bucket, Description) 
+    }
+    # Colorize if we are not in raw mode
+    $pattern | % {
+        $pattern_ = $_
+        Foreach ($line in $table) {
+            $line.Name = $line.Name -replace "($pattern_)", "$cMatch`$1$cNormal"
+            if (!$oName) {
+                $line.Description = $line.Description -replace "($pattern_)", "$cMatch`$1$cNormal"
+            }
+        }
+    }
+    $table | Select-Object Name, Version, Bucket, Description |  Format-Table
+}
+return ss @args
